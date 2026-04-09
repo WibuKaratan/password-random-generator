0e4bffaf09046b1d8e331d8d69aa25850b47b848
WibuKaratan <wibukaratans@gmail.com>
2026-04-09 23:08:21 +0700

    Clear Keyword

--- package/keyword.py
+++ package/keyword.py
@@ -1,9 +1,26 @@
+#keyword for main.py
 
-def alphabet() :
-
-   alphabet = [
+def alphabetUpper() :
+   alphabetUpper = [
        'A', 'B', 'C', 'D', 'E', 'F', 'G',
        'H', 'I', 'J', 'K', 'L', 'M', 'N',
        'O', 'P', 'Q', 'R', 'S', 'T', 'U',
        'V', 'W', 'X', 'Y', 'Z'
    ]
+   return
+
+
+def alphabetLower() :
+   alphabetLower = [
+       'a', 'b', 'c', 'd', 'e', 'f', 'g',
+       'h', 'i', 'j', 'k', 'l', 'm', 'n',
+       'o', 'p', 'q', 'r', 's', 't', 'u',  
+       'v', 'w', 'x', 'y', 'z'
+   ]    
+   return
+
+def number():
+   number = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+   ]
+   return 
