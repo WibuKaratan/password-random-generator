feae8861f56bcfa43cebd74d687b9e90276e01a6
WibuKaratan <wibukaratans@gmail.com>
2026-04-13 21:38:29 +0700

    kapan kapan gw lanjutin kalau inget hehe

---
+++ main.py
@@ -0,0 +1,5 @@
+import random
+from package.keyword import alphabetUpper as aU
+from package.keyword import alphabetLower as aL
+from package.keyword import numberKeyword as nK
+
--- package/keyword.py
+++ package/keyword.py
@@ -19,7 +19,7 @@ def alphabetLower() :
    ]    
    return
 
-def number():
+def numberKeyword():
    number = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 0
    ]
